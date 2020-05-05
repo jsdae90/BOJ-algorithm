d88377469c98c3ed34fe841f4c88a9c8b2a14522
jsdae90 <jsdae90@gmail.com>
2020-05-06 01:21:34 +0900

    boj_11655

--- boj_11655.py
+++ boj_11655.py
@@ -1,34 +1,5 @@
 import sys
 
-lower_case = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
-
 upper_case = [
     "A",
     "B",
@@ -56,6 +27,35 @@ upper_case = [
     "X",
     "Y",
     "Z",
+]
+
+lower_case = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
 ]
 
 line = list(sys.stdin.readline().rstrip())
