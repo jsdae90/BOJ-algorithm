fd03ddfd477b67fadfc0d14fd5c02a9810083245
jsdae90 <jsdae90@gmail.com>
2020-05-06 00:59:49 +0900

    boj_11655

---
+++ boj_11655.py
@@ -0,0 +1,77 @@
+import sys
+
+upper_case = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+
+lower_case = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+line = list(sys.stdin.readline().rstrip())
+
+for i in range(len(line)):
+    if line[i] in upper_case:
+        idx = upper_case.index(line[i])
+        idx += 13
+        if idx > 25:
+            idx %= 25 + 1
+        line[i] = upper_case[idx]
+    elif line[i] in lower_case:
+        idx = lower_case.index(line[i])
+        idx += 13
+        if idx > 25:
+            idx %= 25 + 1
+        line[i] = lower_case[idx]
+
+print("".join(line))
